82e2539cc1f47e8ba01216040e13be803f388728
Matias Perez <ext_matperez@mercadolibre.com>
2024-03-03 14:08:29 -0300

    index agregado

--- mi_proyecto/hola_mundo_app/views.py
+++ mi_proyecto/hola_mundo_app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+def index(request):
+    return render(request, 'hola_mundo_app/index.html')
+
 def hola_mundo(request):
     return HttpResponse('Hola Mundo')
+
+
