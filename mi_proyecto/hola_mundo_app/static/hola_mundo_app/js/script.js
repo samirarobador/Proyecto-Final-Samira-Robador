9291187d1449b4d6b5af5bb350ed078156be59e3
Matias Perez <ext_matperez@mercadolibre.com>
2024-03-05 22:16:43 -0300

    actualizacion

--- mi_proyecto/hola_mundo_app/static/hola_mundo_app/js/script.js
+++ mi_proyecto/hola_mundo_app/static/hola_mundo_app/js/script.js
@@ -1,35 +1,15 @@
 // static/hola_mundo_app/js/script.js
-let var1 = "valor1";
-let var2 = "valor2";
-let var3 = "valor3";
+function verificarCambio() {
+    fetch('/mostrar-texto/')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('texto-actualizado').innerText = data.texto;
+    })
+    .catch(error => console.log('Error:', error));
+}
 
-let url = "/parametrosArticulares/?px=${var1}&py=${var2}&pz=${var3}";
-fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Estoy");
-            if(data.q1 !== undefined) {
-                document.getElementById("labelq1").innerText = "q1: " + data.q1;
-            } else {
-                document.getElementById("labelq1").innerText = "Error: " + data.error;
-            }
-            if(data.q2 !== undefined) {
-                document.getElementById("labelq2").innerText = "q2: " + data.q2;
-            } else {
-                document.getElementById("labelq2").innerText = "Error: " + data.error;
-            }
-            if(data.q3 !== undefined) {
-                document.getElementById("labelq3").innerText = "q3: " + data.q3;
-            } else {
-                document.getElementById("labelq3").innerText = "Error: " + data.error;
-            }
-            if(data.q4 !== undefined) {
-                document.getElementById("labelq4").innerText = "q4: " + data.q4;
-            } else {
-                document.getElementById("labelq4").innerText = "Error: " + data.error;
-            }
-        })
-        .catch(error => console.error('Error:', error));
+// Verifica si hay cambios cada 5 segundos
+setInterval(verificarCambio, 5000);
 
 document.getElementById("parametrosBtn").onclick = function() {
     const num1 = document.getElementById("px").value;
